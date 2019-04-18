d9195aa49c84c0a245050bb76a17300882e97c05
asa1374 <asa1374@gmail.com>
2019-04-18 20:06:23 +0900

    2019.04.18

--- app/src/main/java/com/example/webapp/Index.java
+++ app/src/main/java/com/example/webapp/Index.java
@@ -4,16 +4,22 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
-import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class Index extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Context _this = Index.this;
+        //setContentView(R.layout.index);
+        //WebView webView = findViewById(R.id.webView);
+        //webView.setWebViewClient(new WebViewClient());
+        //webView.loadUrl("https://www.naver.com/");
+        final Context _this = Index.this;
         LinearLayout frame = new LinearLayout(_this);
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
@@ -22,7 +28,20 @@ public class Index extends AppCompatActivity {
         frame.setLayoutParams(params);
         WebView webView = new WebView(_this);
         webView.setLayoutParams(params);
-        webView.setWebChromeClient(new WebChromeClient());
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setUseWideViewPort(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.addJavascriptInterface(new Object(){
+            @android.webkit.JavascriptInterface
+            public void showToast(String msg){
+                Toast.makeText(_this,msg, Toast.LENGTH_LONG).show();
+            }
+            @android.webkit.JavascriptInterface
+            public int sum(String num1,String num2){
+                return Integer.parseInt(num1) + Integer.parseInt(num2);
+            }
+        },"hybrid");
         webView.loadUrl("file:///android_asset/html/index.html");
         frame.addView(webView);
         setContentView(frame);
